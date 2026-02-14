4761c6a0357fe62aa66295c60609316abaacc717
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-14 18:32:44 +0545

    added suspicious keywords and legitimate TLDs

--- phishing_detector.py
+++ phishing_detector.py
@@ -3,7 +3,9 @@ import numpy as np
 class ULRFeatureExtractor:
 
     def __init__(self):
-        pass
+        self.sus_keywords = ['urgent', 'required', 'invoice', 'account update', 'password', 'confirm', 'lottery', 'login', 'signin', 'banking', 'money', 'action', 'file', 'request']
+
+        self.good_tlds = ['.com', '.gov', '.org', '.edu', '.net']
 
     """This function extracts numerical and symbolic features from URL"""
     def extract_feature():
