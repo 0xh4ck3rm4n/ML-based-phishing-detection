cacda1dc303770ff12fe1f9b7a8c84b1f04ffe49
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-14 18:39:35 +0545

    updated phishing detector script with entropy calculator

--- phishing_detector.py
+++ phishing_detector.py
@@ -7,6 +7,22 @@ class ULRFeatureExtractor:
 
         self.good_tlds = ['.com', '.gov', '.org', '.edu', '.net']
 
-    """This function extracts numerical and symbolic features from URL"""
+    #[TODO: This function extracts numerical and symbolic features from URL]
     def extract_feature():
         pass
+
+    # This function calculates the shannon entropy of the text
+    def calculate_entropy(self, text: str) -> float:
+        if not text:
+            return 0.0
+        prob_dict = {}
+        for char in text:
+            prob_dict[char] = prob_dict.get(char, 0) + 1
+        
+        entropy = 0.0
+        text_len = len(text)
+        for count in prob_dict.values():
+            prob = count / text_len
+            entropy -= prob * np.log2(prob)
+        
+        return entropy
