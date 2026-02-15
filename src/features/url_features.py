a23ade482621a793cb9ce5f46dccc67edfbcd661
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-15 11:22:53 +0545

    updated URL feature script with function skeleton

---
+++ src/features/url_features.py
@@ -0,0 +1,48 @@
+class URLFeatureExtractor:
+    def __init__(self):
+        pass
+    
+    # This function extract all features from the URL
+    def extract():
+        pass
+    
+    # This function extract features from multiple URLs
+    def extract_batch():
+        pass
+
+    def calculate_entropy():
+        pass
+
+    def count_subdomains():
+        pass
+
+    def get_domain_length():
+        pass
+
+    def get_path_length():
+        pass
+
+    def has_shortening_service():
+        pass
+
+    def domain_in_path():
+        pass
+
+    def count_special_character():
+        pass
+    
+    # This function checks for suspicous port number
+    def has_suspicious_port():
+        pass
+
+    # This function checks for prefix / suffix seperators in domain
+    def prefix_suffix_in_domain():
+        pass
+    
+    # This function detects the abnormal URL pattern
+    def abnormal_url():
+        pass
+
+    # This function gets the name of all extracted features
+    def get_features_name():
+        pass
