863789c7427b7196f794113b6d233bf51dc957e1
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-15 20:06:24 +0545

    created text feature extractor skeleton functions

---
+++ src/features/text_features.py
@@ -0,0 +1,33 @@
+
+class TextFeatureExtractor:
+    
+    def __init__():
+        pass
+    
+    # This function initializes transformer model (BERT) for embeddings
+    def _init_transformer():
+        pass
+    
+    # This function fit the TF-IDF vectorizer on training texts
+    def fit():
+        pass
+    
+    # This function extract statistical features from text
+    def extract_statistical():
+        pass
+
+    # This function extract TF-IDF features from texts
+    def extract_tfidf():
+        pass
+    
+    # This function is used to extract embeddings using transformer model (BERT)
+    def extract_transformer_embeddings():
+        pass
+    
+    # This function extract combined features: statistical + TF-IDF
+    def extract_combined():
+        pass
+    
+    # This function get names of statistical features
+    def get_feature_names():
+        pass
